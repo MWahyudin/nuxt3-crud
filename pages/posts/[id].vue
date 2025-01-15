db598f170066e34a0281af02d3aa3da255e72f37
clarina03 <clarinamonicadestin3@gmail.com>
2025-01-15 09:11:58 +0700

    Add: fitur menampilkan detail post

---
+++ pages/posts/[id].vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import api from '~/services/api';
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+  const post = ref({});
+  
+  onMounted(async () => {
+    const { data } = await api.get(`/posts/${route.params.id}`);
+    post.value = data;
+  });
+  </script>
+  
