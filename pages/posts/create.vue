158106e724d94bdf41bec618873c839c7d5526c6
clarina03 <clarinamonicadestin3@gmail.com>
2025-01-15 09:06:51 +0700

    Add: fitur menambahkan post baru

---
+++ pages/posts/create.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Create Post</h1>
+      <form @submit.prevent="createPost">
+        <label>Title:</label>
+        <input v-model="title" required />
+        <label>Body:</label>
+        <textarea v-model="body" required></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import api from '~/services/api';
+  
+  const title = ref('');
+  const body = ref('');
+  
+  const createPost = async () => {
+    const response = await api.post('/posts', { title: title.value, body: body.value });
+    alert('Post Created: ' + response.data.id);
+  };
+  </script>
+  
